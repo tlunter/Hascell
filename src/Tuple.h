9652ec6b6681a796be2047f6cf27bde8dc93dfa9
Tom Hulihan <hulihan.tom159@gmail.com>
2012-10-21 14:47:36 -0400

    added comments and a copy constructor to Tuple

--- src/Tuple.h
+++ src/Tuple.h
@@ -1,3 +1,11 @@
+/* This class is for passing around multiple values of different types. It has
+ * only two functions, first and second, which returns those respecitve values
+ * in the tuple.
+ *
+ * Created on October 21, 2011
+ * Author: Tom Hulihan
+ * 
+ **/
 #ifndef _TUPLE_H_
 #define _TUPLE_H_
 
@@ -8,8 +16,10 @@ template <typename A, typename B>
 class Tuple : public Showable
 {
     public:
+
         Tuple() : fst(A()), snd(B()) {}
         Tuple(A f, B s) : fst(f), snd(s) {}
+        Tuple(const Tuple& t) : fst(t.fst), snd(t.snd) {}
 
         A first()
         {
